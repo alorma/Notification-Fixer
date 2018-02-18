8b5427cf6a835ae9409cc7a6b7b99f5b78d10038
Bernat Borrás <bernatbor15@gmail.com>
2018-02-18 11:33:19 +0100

    Layout && back

--- app/src/main/java/com/alorma/notifix/ui/features/trigger/CreateTriggerActivity.kt
+++ app/src/main/java/com/alorma/notifix/ui/features/trigger/CreateTriggerActivity.kt
@@ -1,6 +1,20 @@
 package com.alorma.notifix.ui.features.trigger
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.alorma.notifix.R
+import com.alorma.notifix.ui.utils.dsl
+import kotlinx.android.synthetic.main.activity_add_trigger.*
 
 class CreateTriggerActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_trigger)
+
+        toolbar.dsl {
+            back { action = { finish() } }
+        }
+    }
+
 }
