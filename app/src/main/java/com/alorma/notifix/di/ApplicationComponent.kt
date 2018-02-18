638e30dff8350fbde8754a22acccda483b6438bf
Bernat Borrás <bernatbor15@gmail.com>
2018-02-18 11:45:54 +0100

    Add action selector listener

--- app/src/main/java/com/alorma/notifix/di/ApplicationComponent.kt
+++ app/src/main/java/com/alorma/notifix/di/ApplicationComponent.kt
@@ -5,6 +5,7 @@ import com.alorma.notifix.di.module.ApplicationModule
 import com.alorma.notifix.di.module.DataModule
 import com.alorma.notifix.ui.features.create.AddNotificationActivity
 import com.alorma.notifix.ui.features.notifications.MainActivity
+import com.alorma.notifix.ui.features.trigger.CreateTriggerActivity
 import dagger.Component
 import javax.inject.Singleton
 
@@ -13,6 +14,7 @@ import javax.inject.Singleton
 interface ApplicationComponent {
     infix fun inject(activity: MainActivity)
     infix fun inject(activity: AddNotificationActivity)
+    infix fun inject(activity: CreateTriggerActivity)
 
     infix fun inject(bootBroadcast: NotificationsBootBroadcast)
 }
--- app/src/main/java/com/alorma/notifix/ui/features/trigger/CreateTriggerActivity.kt
+++ app/src/main/java/com/alorma/notifix/ui/features/trigger/CreateTriggerActivity.kt
@@ -2,18 +2,43 @@ package com.alorma.notifix.ui.features.trigger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import com.alorma.notifix.NotifixApplication.Companion.component
 import com.alorma.notifix.R
+import com.alorma.notifix.data.Logger
 import com.alorma.notifix.ui.utils.dsl
 import kotlinx.android.synthetic.main.activity_add_trigger.*
+import javax.inject.Inject
 
-class CreateTriggerActivity: AppCompatActivity() {
+class CreateTriggerActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_trigger)
 
+        component inject this
+
         toolbar.dsl {
             back { action = { finish() } }
+        }
+
+        actionSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> logger.i("SMS")
+                    1 -> logger.i("PHONE")
+                    2 -> logger.i("TIME")
+                    3 -> logger.i("ZONE")
+                }
+            }
         }
     }
 
